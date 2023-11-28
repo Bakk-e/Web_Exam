6c42534cb735df34cb69e96ccbffc81308d9b3de
binnama <laasestad@yahoo.no>
2023-11-28 11:46:19 +0100

    Push before fetch/merge

--- oppgave_2/src/app/api/athletes/[athleteId]/route.ts
+++ oppgave_2/src/app/api/athletes/[athleteId]/route.ts
@@ -21,7 +21,7 @@ const prisma = new PrismaClient();
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     // Fetch data from external API
-    const apiUrl = "https://webapp-api.vercel.app/api/users"
+    //const apiUrl = "https://webapp-api.vercel.app/api/users"
     const response = await fetch(apiUrl)
 
     if (!response.ok) {
