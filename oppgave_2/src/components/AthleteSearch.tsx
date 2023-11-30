27871aaa1c7e664d33040c2b6cc04c51b11c4be0
Bakk-e <jorgenba@hiof.no>
2023-11-30 19:29:18 +0100

    clear search

--- oppgave_2/src/components/AthleteSearch.tsx
+++ oppgave_2/src/components/AthleteSearch.tsx
@@ -12,12 +12,28 @@ export default function AthleteSearch(props: athleteSearchProps) {
     const {athletes, onSearch} = props;
     const [searchTerm, setSearchTerm] = useState("");
 
-    function handleSearch(e: any) {
+    function clearSearch(e: any) {
         e.preventDefault();
+
+        setSearchTerm("");
+        handleSearch(e, true);
+    }
+
+    function handleSearch(e: any, clear: boolean) {
+        e.preventDefault();
+
+        let filteredAthletes: AthleteMini[];
+
+        if (!clear) {
+            filteredAthletes = athletes.filter(athlete =>
+                athlete.id.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        } else {
+            filteredAthletes = athletes.filter(athlete =>
+                athlete.id.toLowerCase().includes("")
+            )
+        }
         
-        const filteredAthletes = athletes.filter(athlete =>
-            athlete.id.toLowerCase().includes(searchTerm.toLowerCase())
-        );
         onSearch(filteredAthletes);
     }
 
@@ -28,7 +44,8 @@ export default function AthleteSearch(props: athleteSearchProps) {
             placeholder="Søk"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}/>
-            <button id="main-page-search-button" onClick={handleSearch}>Search</button>
+            <button id="main-page-search-clear" onClick={clearSearch}>x</button>
+            <button id="main-page-search-button" onClick={(e) => handleSearch(e, false)}>Search</button>
         </form>
     )
 }
