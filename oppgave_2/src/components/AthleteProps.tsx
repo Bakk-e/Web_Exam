63ce2278eb742189208d0d8f3c05f76986f55bd0
binnama <laasestad@yahoo.no>
2023-12-01 21:47:19 +0100

    userId showing on index and athlete

---
+++ oppgave_2/src/components/AthleteProps.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+type athleteProps = {
+    userId: string,
+    gender: string,
+    sport: string
+}
+
+export default function Athlete(props: athleteProps) {
+    const {userId, gender, sport} = props;
+
+    return (
+        <tr>
+            <td><Link legacyBehavior href="/athlete/[id]" as={`/athlete/${userId}`}><a>{userId}</a></Link></td>
+            <td>{gender}</td>
+            <td>{sport}</td>
+            <td><Link legacyBehavior href="/reports/[athleteId]" as={`/reports/${userId}`}><a>Klikk her</a></Link></td>
+        </tr>
+    )
+}
