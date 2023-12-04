225b8f64cbb9342fa828b7e190b46b079871635c
Zupar <jacobh@hiof.no>
2023-12-04 22:41:52 +0100

    Implementing create question, also fixing some css on buttons

--- oppgave_2/src/app/newQuestions/page.tsx
+++ oppgave_2/src/app/newQuestions/page.tsx
@@ -3,8 +3,44 @@
 import Notifications from "@/components/Notifications"
 import "@/styles/NewQuestionsPageStyle.css"
 import Link from "next/link"
+import {useState} from "react";
 
 export default function NewQuestionsPage() {
+    const [question,setQuestion] = useState({
+        text : "",
+        type : "Text",
+        answer: "",
+    })
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target //as HTMLInputElement | HTMLSelectElement;
+        setQuestion({ ...question, [name]: value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Submitting question:", question);
+        try {
+            const response = await fetch('/api/question', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(question),
+            });
+            console.log(response)
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            console.log(result);
+        }catch (error) {
+            console.error('Failed to submit question', error);
+        }
+    }
+
+
     return (
         <div id="new-questions-page">
             <header id="new-questions-page-header">
@@ -14,26 +50,41 @@ export default function NewQuestionsPage() {
                     <Notifications></Notifications>
                 </nav>
             </header>
-            <div id="new-questions-page-create">
-                <p id="new-questions-page-title">Opprett spørsmål</p>
-                <div id="new-questions-page-create-table">
-                    <div className="new-questions-page-create-point">
-                        <p className="new-questions-page-create-point-title">Text: </p>
-                        <input className="new-questions-page-create-point-input"/>
-                    </div>
-                    <div className="new-questions-page-create-point">
-                        <p className="new-questions-page-create-point-title">Svar type: </p>
-                        <select id="new-questions-page-create-point-dropdown">
-                            <option value="text">Tekst</option>
-                            <option value="radio">Radio 1-10</option>
-                            <option value="emoji">Emoji</option>
-                        </select>
-                    </div>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="new-questions-page-create-point">
+                    <label className="new-questions-page-create-point-title">Text:</label>
+                    <input
+                        name="text"
+                        value={question.text}
+                        onChange={handleChange}
+                        className="new-questions-page-create-point-input"
+                    />
                 </div>
-            </div>
-            <div id="new-questions-page-save-section">
-                <button id="new-questions-page-save-button">Save</button>
-            </div>
+                <div className="new-questions-page-create-point">
+                    <label className="new-questions-page-create-point-title">Answer Type:</label>
+                    <select
+                        name="type"
+                        value={question.type}
+                        onChange={handleChange}
+                        className="new-questions-page-create-point-dropdown">
+                        <option value="text">Text</option>
+                        <option value="radio">Radio 1-10</option>
+                        <option value="emoji">Emoji</option>
+                    </select>
+                </div>
+                <div className="new-questions-page-create-point">
+                    <label className="new-questions-page-create-point-title">Answer:</label>
+                    <input
+                        name="answer"
+                        value={question.answer}
+                        onChange={handleChange}
+                        className="new-questions-page-create-point-input"
+                    />
+                </div>
+                <div className="new-questions-page-save-section">
+                    <button type="submit" className="new-questions-page-save-button">Save</button>
+                </div>
+            </form>
         </div>
     )
 }
