cfd8350df3ef66623cb67f5577a9d6684091f259
Zupar <jacobh@hiof.no>
2023-12-04 19:35:33 +0100

    fixing create user, and gui

--- oppgave_2/src/app/api/athletes/route.ts
+++ oppgave_2/src/app/api/athletes/route.ts
@@ -4,16 +4,19 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import {randomUUID} from "crypto";
 import {Athlete} from "@/types"
 
-export type AthleteProps = {
-    firstName : string
-    lastName : string
-    id : number
-    gender: string;
-    sport: string;
+export type MetaProps = {
     maxHeartRate: string;
     thresholdWattage: string;
     thresholdSpeed: string;
-}
+};
+
+export type AthleteProps = {
+    firstName: string;
+    lastName: string;
+    gender: string;
+    sport: string;
+    meta: MetaProps;
+};
 
 function generateRandomStringFromName(firstName : string, lastName : string){
     const combinedString = firstName + lastName
@@ -52,41 +55,35 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
 }
 
 export async function POST (req: NextRequest, res: NextResponse) {
-    console.log( "BEFORE BODY");
     const body = await req.json() as AthleteProps
-    console.log( "BODY", body);
-
-    console.log("REQ BODY", req.body)
-    console.log("1")
     try {
-
-        console.log("2")
-        const {firstName, lastName, id , gender, sport, maxHeartRate, thresholdWattage, thresholdSpeed } = body
-
-
-        console.log("3")
-        console.log("4")
-        console.log({ gender, sport, maxHeartRate, thresholdWattage, thresholdSpeed});
+        const { firstName, lastName, gender, sport, meta } = body;
+        console.log({ gender, sport, meta});
         const newAthlete = await prisma.athlete.create({
             data : {
                 id : randomUUID(),
                 userId : generateRandomStringFromName(firstName,lastName),
                 gender : gender,
                 sport : sport,
-                maxHeartRate : maxHeartRate ? parseInt(maxHeartRate,10) : null,
-                thresholdWattage : thresholdWattage? parseInt(thresholdWattage, 10) : null,
-                thresholdSpeed : thresholdSpeed? parseInt(thresholdSpeed, 10) : null
             }
         })
-        return NextResponse.json({success : true ,data :newAthlete, message : "Created new athlete"})
+
+        const newMeta = await prisma.meta.create({
+            data: {
+                heartrate: meta.maxHeartRate ? parseInt(meta.maxHeartRate, 10) : null,
+                watt: meta.thresholdWattage ? parseInt(meta.thresholdWattage, 10) : null,
+                speed: meta.thresholdSpeed ? parseInt(meta.thresholdSpeed, 10) : null,
+                athleteId: newAthlete.id,
+            }
+        });
+        return NextResponse.json({success : true ,data : {newAthlete, meta : newMeta}, message : "Created new athlete"})
         //res.status(200).json(newAthlete)
     }catch (e){
         console.error("Failed to create athlete", e)
-       // res.status(500).json({ error: "Internal Server Error" });
+        // res.status(500).json({ error: "Internal Server Error" });
         return NextResponse.json({success : false , message : "Internal Server Error"})
     }
 }
-
 /*
 export async function POST(request: NextRequest) {
     if (request.method === "POST") {
